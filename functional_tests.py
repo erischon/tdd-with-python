ede19921d9700d2e8f06dce10916f96b6f87b049
Eri Schon <erischon@gmail.com>
2021-02-22 12:33:23 +0100

    Update functional_tests.py

--- functional_tests.py
+++ functional_tests.py
@@ -1,6 +1,16 @@
 from selenium import webdriver
 
 browser = webdriver.Firefox()
+
+# Edith has heard about a cool new online to-do app.
+# She goes to check out its homepage
 browser.get('http://localhost:8000')
 
-assert 'Django' in browser.title
+# She notices the page title and header mention to-do lists
+assert 'To-Do' in browser.title
+
+# She is invited to enter a to-do item straight away
+
+# 
+
+browser.quit()
