ff4e2960f0989d440a0e2be26001241d80884e96
Eri Schon <erischon@gmail.com>
2021-04-07 17:00:31 +0200

    Create tests.py

---
+++ accounts/tests.py
@@ -0,0 +1,35 @@
+from django.test import TestCase, Client
+from django.urls import reverse
+import accounts.views
+
+
+class SendLoginEmailViewTest(TestCase):
+
+    # def test_redirects_to_home_page(self):
+    #     self.client = Client()
+    #     response = self.client.post(reverse('send_login_email'))
+    #     self.assertRedirects(response, '/')
+
+    def test_sends_mail_to_address_from_post(self):
+        self.send_mail_called = False
+
+        def fake_send_mail(subject, body, from_email, to_list):  
+            self.send_mail_called = True
+            self.subject = subject
+            self.body = body
+            self.from_email = from_email
+            self.to_list = to_list
+
+        accounts.views.send_mail = fake_send_mail  
+
+        self.client.post(reverse('send_login_email'), data={
+            'email': 'edith@example.com'
+        })
+
+        self.assertTrue(self.send_mail_called)
+        self.assertEqual(self.subject, 'Your login link for Superlists')
+        self.assertEqual(self.from_email, 'noreply@superlists')
+        self.assertEqual(self.to_list, ['edith@example.com'])
+
+
+ 
